cd81dc79754f65151163eb737267369f62b7708e
Simon <simon.dimas@g.austincc.edu>
2020-01-27 18:16:38 -0600

    Hello World but with functions.

--- src/examples/01_module/01_output/output.cpp
+++ src/examples/01_module/01_output/output.cpp
@@ -2,4 +2,10 @@
 
 #include <iostream>
 
+using std::cout; //If you'll use it multiple times
 
+void say_hello()
+{
+	//std::cout << "Hello World!"; If you're going to use it once
+	cout << "Hello World!";
+}
