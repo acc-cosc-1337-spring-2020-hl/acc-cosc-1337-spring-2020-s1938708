725821761ec4e3d64f52da3fce8ed1c01c353d09
Simon Dimas <simon.dimas@g.austincc.edu>
2020-04-13 19:24:01 -0500

    Learned how to Read, input  and output text files

--- src/examples/09_module/main.cpp
+++ src/examples/09_module/main.cpp
@@ -1,6 +1,18 @@
-
+#include "temperature_data.h"
 int main() 
 {
+	std::vector<Temperature> out_temps{ Temperature(1, 99.9), Temperature(2, 98.5), Temperature(3, 101.1),
+										Temperature(4, 98.9), Temperature(5, 102.5), Temperature(6, 98.4) };
 
+	TemperatureData data;
+
+	data.save_temps(out_temps);
+
+	std::vector<Temperature> in_temps = data.get_temps();
+
+	for (auto temp : in_temps)
+	{
+		std::cout << temp.get_hour() << " " << temp.get_reading() << "\n";
+	}
 	return 0;
 }
--- src/examples/09_module/temperature_data.cpp
+++ src/examples/09_module/temperature_data.cpp
@@ -1,1 +1,40 @@
 //temperature_data.cpp
+#include "temperature_data.h"
+
+void TemperatureData::save_temps(std::vector<Temperature>& ts)
+{
+	std::ofstream file_out(file_name, std::ios_base::trunc);
+
+	for (auto temp : ts)
+	{
+		file_out << temp.get_hour();
+		file_out << " ";
+		file_out << temp.get_reading();
+		file_out << "\n";
+	}
+
+	file_out.close();
+}
+
+std::vector<Temperature> TemperatureData::get_temps() const
+{
+	std::vector<Temperature> temps;
+
+	std::ifstream read_file(file_name);
+
+	int hour;
+	double temp;
+
+	if (read_file.is_open())
+	{
+		while (read_file >> hour >> temp)
+		{
+			Temperature t(hour, temp);
+			temps.push_back(t);
+		}
+	}
+
+	read_file.close();
+
+	return temps;
+}
